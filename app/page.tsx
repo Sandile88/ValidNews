16bf3488c78dd34aeb2837b8144d897480c5fdb2
Sandile88 <sandilep.mremi@gmail.com>
2025-10-15 14:52:09 +0200

    fixed wallet wrapper import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 import Image from "next/image";
 import styles from "./page.module.css";
-import WalletWrapper from "../components/WalletWrapper";
+import WalletWrapper from "./components/WalletWrapper";
 
 export default function Home() {
   return (
