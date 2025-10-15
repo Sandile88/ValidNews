b65683db1c33b89cb13a3f60ecdb27ed93825cae
Sandile88 <sandilep.mremi@gmail.com>
2025-10-15 15:24:15 +0200

    package json fixes

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,6 @@ const nextConfig: NextConfig = {
   transpilePackages: ["@coinbase/onchainkit"],
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
-
-     config.resolve.fallback = {
-      ...config.resolve.fallback,
-      "@react-native-async-storage/async-storage": false,
-
-    };
-
     return config;
   },
 };
